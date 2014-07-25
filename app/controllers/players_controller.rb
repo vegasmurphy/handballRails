ac35556f087a28201c7dcbba7388d0d749866118
Vegas Murphy <Vegas.Murphy@wnco.com>
2014-07-25 07:33:22 -0500

    Search capital letters fix players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -25,7 +25,7 @@ class PlayersController < ApplicationController
   # POST /players.json
   def create
     @player = Player.new(player_params)
-
+    @player.name=@player.name.titleize
     respond_to do |format|
       if @player.save
         format.html { redirect_to @player, notice: 'Player was successfully created.' }
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-  	@results = Player.search params["query"]
+  	@results = Player.search params["query"].titleize
   end
 
 end
